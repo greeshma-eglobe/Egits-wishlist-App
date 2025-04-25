c98ceb388a17bd4ed99f87086fb475a9bbc7d9b8
Greeshma <greeshma.s@eglobeits.com>
2025-04-25 15:02:48 +0530

    - Add wishlist icon to all products using Theme App Extension.

---
+++ app/routes/api.getProductTags.jsx
@@ -0,0 +1,27 @@
+import { json } from "@remix-run/node";
+import { unauthenticated } from "../shopify.server";
+import { cors } from "remix-utils/cors";
+import db from "../db.server";
+
+export async function loader({ request }) {
+  const url = new URL(request.url);
+   const shop = url.searchParams.get("shop");
+
+    try {
+      // Fetch wishlist items
+      const productTags = await db.productTags.findMany({
+        where: { shop },
+      });
+ 
+      return json(
+        {  productTags: productTags },
+        { headers: { "Access-Control-Allow-Origin": "*" } },
+      );
+  
+    } catch (error) {
+      return json(
+        { message: "Error fetching productTags", error: error.message },
+        { status: 500 }
+      );
+    }
+}
